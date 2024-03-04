3dbe3cd736fa7b75b98f14d8168a61288561db88
Jackson Windhorst <162068481+axnjxn7@users.noreply.github.com>
2024-03-03 23:44:38 -0500

    Rule 02.01 Do not use a null in a case where an object is required
    This compliant solution eliminates the null pointer dereference by adding an explicit check.

--- R02_XP01_J.java
+++ R02_XP01_J.java
@@ -2,18 +2,18 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public class R02_XP01_J {
-    public static int cardinality(Object obj, final Collection<?> col) {
-        int count = 0;
-        if (col == null) {
-            return count;
-        }
-        Iterator<?> it = col.iterator();
-        while (it.hasNext()) {
-            Object elt = it.next();
-            if ((obj == null && elt == null) || (obj != null && obj.equals(elt))) { // Null pointer dereference
-                count++;
-            }
-        }
+  public static int cardinality(Object obj, final Collection col) {
+      int count = 0;
+      if (col == null) {
         return count;
-    }
+      }
+      Iterator it = col.iterator();
+      while (it.hasNext()) {
+        Object elt = it.next();
+        if ((null == obj && null == elt) || (null != obj && obj.equals(elt))) {
+          count++;
+        }
+      }
+      return count;
+  }
 }
