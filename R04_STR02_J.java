b24302fc6a6d7f49f41c94b6d436e85020802d89
Jackson Windhorst <162068481+axnjxn7@users.noreply.github.com>
2024-03-03 23:47:43 -0500

    Rule 004.02 Specify an appropriate locale when comparing locale-dependent data
    This compliant solution explicitly sets the locale to English to avoid unexpected results.

--- R04_STR02_J.java
+++ R04_STR02_J.java
@@ -1,6 +1,6 @@
 public class R04_STR02_J {
     public static void processTag(String tag) {
-        if (tag == null || tag.equalsIgnoreCase("SCRIPT")) {
+        if (tag.toUpperCase(Locale.ENGLISH).equals("SCRIPT")) {
             return;
         }
         // Process tag
